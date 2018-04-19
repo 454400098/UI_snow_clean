12fee26b6f462365c50fdd430c81a91cd81e9aa8
郑皓东 <a454400098@icloud.com>
2018-04-19 10:14:35 -0400

    create two input form for UI

--- mysite/snow/forms.py
+++ mysite/snow/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
-from .models import Snow
+from .models import Snow, Snowcar
+from django import forms
 
 class SnowForm(ModelForm):
     class Meta:
         model = Snow
         fields = ['title']
 
+class Snowcarform(ModelForm):
+    class Meta:
+        model = Snowcar
+        fields = ['carnum']
+
+class Snowdata(forms.Form):
+    address = forms.CharField(max_length=30)
+    numcar = forms.CharField(max_length=10)
--- mysite/snow/views.py
+++ mysite/snow/views.py
@@ -2,23 +2,33 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .forms import SnowForm
+from .forms import SnowForm,Snowcarform, Snowdata
 from .models import Snow
 # Create your views here.
 
 
 
+#def add_snow(request):
+#    if request.method == "POST":
+#        form1 = SnowForm(request.POST)
+ #       if form1.is_valid():
+#            snow_item=form1.save(commit=False)
+#            snow_item.save()
+
+
+#    else:
+#        form1 = SnowForm()
+ #   return render(request,'snow/snow_form.html', {'form':form1})
+
+
 def add_snow(request):
     if request.method == "POST":
-        form = SnowForm(request.POST)
+        form = Snowdata(request.POST)
         if form.is_valid():
-            snow_item=form.save(commit=False)
-            snow_item.save()
-
+            pass
     else:
-        form = SnowForm()
+        form=Snowdata()
     return render(request,'snow/snow_form.html', {'form':form})
-
 
 def edit_snow(request,id=None):
     item = get_object_or_404(Snow,id=id) #return object with id or raist 404 error
